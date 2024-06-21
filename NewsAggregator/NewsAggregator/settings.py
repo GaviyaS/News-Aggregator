cbfaadca3d724a303c16681307dd00f653bf4de6
GaviyaS <141805205+GaviyaS@users.noreply.github.com>
2024-06-21 14:36:32 +0530

    Update settings.py

--- NewsAggregator/NewsAggregator/settings.py
+++ NewsAggregator/NewsAggregator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xe4hmfwal+58e7@8u@&g^x2xsow_hyv7%m3wmk^o4u(%+!73cw'
+SECRET_KEY = 'YOUR KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
